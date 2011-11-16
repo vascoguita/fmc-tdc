fea36da15faebc79c4de4406472e80506b880969
penacoba <penacoba@85dfdc96-de2c-444c-878d-45b388be74a9>
2011-11-16 20:23:45 +0000

    Syn folder after manual cleanup to remove svn lock
    git-svn-id: http://svn.ohwr.org/fmc-tdc@74 85dfdc96-de2c-444c-878d-45b388be74a9

---
+++ hdl/spec/syn/scripts_for_hardware_test/rdbk_config.tcl
@@ -0,0 +1,15 @@
+rrcmd r 0:80040
+rrcmd r 0:80044
+rrcmd r 0:80048
+rrcmd r 0:8004C
+rrcmd r 0:80050
+rrcmd r 0:80054
+rrcmd r 0:80058
+rrcmd r 0:8005C
+rrcmd r 0:8006C
+rrcmd r 0:80070
+rrcmd r 0:80078
+
+rrcmd r 0:80060
+rrcmd r 0:80064
+rrcmd r 0:80068
---
+++ hdl/spec/syn/test_tdc_acam/config1.tcl
@@ -0,0 +1,27 @@
+rrcmd w 0:80000 0000FC81
+rrcmd w 0:80004 00000000
+rrcmd w 0:80008 00000E02
+rrcmd w 0:8000C 00000000
+rrcmd w 0:80010 02000064
+rrcmd w 0:80014 080003E8
+rrcmd w 0:80018 000000FF
+rrcmd w 0:8001C 00001FEA
+rrcmd w 0:8002C 00FF0000
+rrcmd w 0:80030 04000000
+rrcmd w 0:80038 00000000
+
+rrcmd r 0:80000
+rrcmd r 0:80004
+rrcmd r 0:80008
+rrcmd r 0:8000C
+rrcmd r 0:80010
+rrcmd r 0:80014
+rrcmd r 0:80018
+rrcmd r 0:8001C
+rrcmd r 0:8002C
+rrcmd r 0:80030
+rrcmd r 0:80038
+
+rrcmd r 0:80020
+rrcmd r 0:80024
+rrcmd r 0:80028
