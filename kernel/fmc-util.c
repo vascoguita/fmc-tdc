9e9c05d30c9126912e9ef096b7757290ae896e74
Federico Vaga <federico.vaga@cern.ch>
2014-08-12 15:31:48 +0200

    kernel: remove white space
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- kernel/fmc-util.c
+++ kernel/fmc-util.c
@@ -42,7 +42,7 @@ static int traverse_sdb_devices(struct sdb_array *tree,
 			if(rv > 0)
 				return 1;
 		}
-		
+
 		if (r->empty.record_type != sdb_type_device)
 			continue;
 
@@ -52,7 +52,7 @@ static int traverse_sdb_devices(struct sdb_array *tree,
 		vid = __be64_to_cpu(p->vendor_id);
 		did = __be32_to_cpu(p->device_id);
 		size = (uint32_t) (last + 1 - first);
-		
+
 		if (cb (first + tree->baseaddr, size, vid, did))
 		    return 1;
 	}
@@ -60,7 +60,7 @@ static int traverse_sdb_devices(struct sdb_array *tree,
 }
 
 /* Finds the Nth SDB device that matches (vid/did) pair, where N <= *ordinal.
-   If N < *ordinal, the value of N is stored at *ordinal. 
+   If N < *ordinal, the value of N is stored at *ordinal.
    This magic is used to handle hybrid bistreams (with two or more different
    mezzanines). */
 
@@ -69,14 +69,14 @@ signed long fmc_sdb_find_nth_device (struct sdb_array *tree, uint64_t vid, uint3
     int n = -1;
     uint32_t current_address;
     uint32_t current_size;
-    
+
     int callback (uint32_t address, uint32_t size, uint64_t vid_, uint32_t did_)
     {
 	if(vid_ == vid && did_ == did)
 	{
 	    n++;
 	    current_address = address;
-	    current_size = size;    
+	    current_size = size;
 
 	    if(!ordinal || n == *ordinal)
 	    {
@@ -97,6 +97,6 @@ signed long fmc_sdb_find_nth_device (struct sdb_array *tree, uint64_t vid, uint3
 
 	return current_address;
     }
-    
+
     return -ENODEV;
 }
