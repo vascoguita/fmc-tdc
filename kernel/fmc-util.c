eb6ded9600d41d58ef350e57cac945026e4a3e27
Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
2014-08-12 16:39:40 +0200

    kernel: fix fmc find device
    The usage of on-stack-variable by the callback function was a problem
    on some kernels (e.g. 3.6 64bit)
    
    Federico: again I'm including this patch because we have not time to
    wait the proper solution that will come on october 2014 (hopefully)
    
    Signed-off-by: Tomasz Wlostowski <tomasz.wlostowski@cern.ch>
    Acked-off-by: Federico Vaga <federico.vaga@cern.ch>

--- kernel/fmc-util.c
+++ kernel/fmc-util.c
@@ -18,10 +18,11 @@
 
 #include "fmc-tdc.h"
 
-typedef int (*sdb_traverse_cb) (uint32_t address, uint32_t size, uint64_t vid, uint32_t did);
+typedef int (*sdb_traverse_cb) (uint32_t address, uint32_t size, uint64_t vid, uint32_t did, void *data);
 
 static int traverse_sdb_devices(struct sdb_array *tree,
-				sdb_traverse_cb cb)
+				sdb_traverse_cb cb,
+				void *data)
 {
 	union sdb_record *r;
 	struct sdb_product *p;
@@ -38,7 +39,7 @@ static int traverse_sdb_devices(struct sdb_array *tree,
 
 		if (!IS_ERR(tree->subtree[i]))
 		{
-			rv = traverse_sdb_devices ( tree->subtree[i], cb );
+			rv = traverse_sdb_devices ( tree->subtree[i], cb, data );
 			if(rv > 0)
 				return 1;
 		}
@@ -53,10 +54,36 @@ static int traverse_sdb_devices(struct sdb_array *tree,
 		did = __be32_to_cpu(p->device_id);
 		size = (uint32_t) (last + 1 - first);
 
-		if (cb (first + tree->baseaddr, size, vid, did))
+		if (cb (first + tree->baseaddr, size, vid, did, data))
 		    return 1;
 	}
     return 0;
+}
+
+struct callback_state {
+    int n;
+    int *ordinal;
+    uint32_t current_address;
+    uint32_t current_size;
+    uint64_t did;
+    uint32_t vid;
+};
+
+static int callback (uint32_t address, uint32_t size, uint64_t vid_, uint32_t did_, void *data)
+{
+	struct callback_state *st = (struct callback_state *) data;
+	if(vid_ == st->vid && did_ == st->did)
+	{
+	    st->n++;
+	    st->current_address = address;
+	    st->current_size = size;
+
+	    if(!st->ordinal || st->n == *st->ordinal)
+	    {
+		return 1;
+	    }
+	}
+	return 0; /* continue scanning	*/
 }
 
 /* Finds the Nth SDB device that matches (vid/did) pair, where N <= *ordinal.
@@ -66,36 +93,23 @@ static int traverse_sdb_devices(struct sdb_array *tree,
 
 signed long fmc_sdb_find_nth_device (struct sdb_array *tree, uint64_t vid, uint32_t did, int *ordinal, uint32_t *size )
 {
-    int n = -1;
-    uint32_t current_address;
-    uint32_t current_size;
+    struct callback_state st;
 
-    int callback (uint32_t address, uint32_t size, uint64_t vid_, uint32_t did_)
-    {
-	if(vid_ == vid && did_ == did)
-	{
-	    n++;
-	    current_address = address;
-	    current_size = size;
+    st.n = -1;
+    st.ordinal = ordinal;
+    st.vid = vid;
+    st.did = did;
 
-	    if(!ordinal || n == *ordinal)
-	    {
-		return 1;
-	    }
-	}
-	return 0; /* continue scanning	*/
-    }
+    traverse_sdb_devices (tree, callback, &st);
 
-    traverse_sdb_devices (tree, callback);
-
-    if (n >= 0)
+    if (st.n >= 0)
     {
 	if(size)
-	    *size = current_size;
+	    *size = st.current_size;
 	if(ordinal)
-	    *ordinal = n;
+	    *ordinal = st.n;
 
-	return current_address;
+	return st.current_address;
     }
 
     return -ENODEV;
