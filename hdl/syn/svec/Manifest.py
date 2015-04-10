8600d4e25a9e76f3032b16049aa6b6fcfd281224
Tomasz Włostowski <tomasz.wlostowski@cern.ch>
2015-04-10 14:22:23 +0200

    hdl/syn/svec: added missing Manifest file

---
+++ hdl/syn/svec/Manifest.py
@@ -0,0 +1,12 @@
+target = "xilinx"
+action = "synthesis"
+
+fetchto = "../../ip_cores"
+
+syn_device = "xc6slx150t"
+syn_grade = "-3"
+syn_package = "fgg900"
+syn_top = "wr_svec_tdc"
+syn_project = "wr_svec_tdc.xise"
+
+modules = { "local" : [ "../../top/svec" ] }
