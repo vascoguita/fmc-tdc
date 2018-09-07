654b5bab1f4547d7c7eddd2598a949c16536f1f1
Federico Vaga <federico.vaga@cern.ch>
2018-09-07 17:47:47 +0200

    ut: add skeleton
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

---
+++ unittest/fmctdc-ut.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2018 CERN (www.cern.ch)
+ * Author: Federico Vaga <federico.vaga@cern.ch>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	exit(EXIT_SUCCESS);
+}
