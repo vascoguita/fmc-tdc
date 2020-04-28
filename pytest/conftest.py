2806ac241e73e897bcec2389aa1ce66a223a01f8
Federico Vaga <federico.vaga@cern.ch>
2020-04-28 11:36:40 +0200

    tst: add fine-delay pulse generator class
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -5,6 +5,29 @@ SPDX-FileCopyrightText: 2020 CERN
 
 import pytest
 import PyFmcTdc
+import subprocess
+
+class FmcFineDelay(object):
+    def __init__(self, device_id):
+        self.dev_id = device_id
+
+    def generate_pulse(self, ch, rel_time_us,
+                       period_ns, count):
+        cmd = ["/usr/local/bin/fmc-fdelay-pulse",
+               "-d", "0x{:x}".format(self.dev_id),
+               "-o", str(ch),
+               "-r", "{:d}u".format(rel_time_us),
+               "-T", "{:d}n".format(period_ns),
+               "-c", str(count),
+               "-t"
+               ]
+        proc = subprocess.Popen(cmd)
+        proc.wait()
+
+
+@pytest.fixture(scope="module")
+def fmcfd():
+    return FmcFineDelay(pytest.fd_id)
 
 @pytest.fixture(scope="module")
 def fmctdc():
@@ -13,9 +36,9 @@ def fmctdc():
 def pytest_addoption(parser):
     parser.addoption("--tdc-id", action="store", type=lambda x : int(x, 16),
                      required=True)
-    # parser.addoption("--fd-id", action="store", type=lambda x : int(x, 16),
-    #                  required=True)
+    parser.addoption("--fd-id", action="store", type=lambda x : int(x, 16),
+                     required=True)
 
 def pytest_configure(config):
     pytest.tdc_id = config.getoption("--tdc-id")
-    # pytest.fd_id = config.getoption("--fd-id")
+    pytest.fd_id = config.getoption("--fd-id")
