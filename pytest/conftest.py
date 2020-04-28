66f0cb38a2945768dd434236e2f707402cb893de
Federico Vaga <federico.vaga@cern.ch>
2020-04-28 18:00:17 +0200

    tst: remove TDC options (use a dedicate script)
    It is impossible to do it nicely because each system has its own
    options and we can't list them all here.  It is better to have a
    separated script that does the installation.
    
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- pytest/conftest.py
+++ pytest/conftest.py
@@ -45,52 +45,11 @@ def fmctdc():
         tdc.chan[i].flush()
 
 def pytest_addoption(parser):
-    parser.addoption("--base-root",  default="/")
-    parser.addoption("--carrier", choices=["spec", "svec"], required=True)
-    parser.addoption("--tdc-bitstream", default=None)
     parser.addoption("--tdc-id", type=lambda x : int(x, 16),
                      required=True)
     parser.addoption("--fd-id", type=lambda x : int(x, 16),
                      required=True)
 
-def fmctdc_configure_drivers(config):
-    drv_list = []
-    if config.getoption("--carrier") == "spec":
-        drv_list += ["zio-buf-vmalloc", "spec-fmc-carrier", "fmc-tdc-spec",
-                     "fmc-fine-delay-spec"]
-    elif config.getoption("--carrier") == "svec":
-        drv_list += ["zio-buf-vmalloc", "svec-fmc-carrier", "fmc-tdc-spev",
-                     "fmc-fine-delay-spev"]
-
-    for drv in drv_list:
-        proc = subprocess.Popen(["modprobe -d {:s} {:s}".format(config.getoption("--base-root"), drv)],
-                                shell=True)
-        proc.wait()
-
-def fmctdc_configure_bitstream(config):
-    bit = config.getoption("--tdc-bitstream")
-    if bit is None:
-        return
-
-    full_path = os.readlink("/sys/bus/zio/devices/tdc-1n5c-{:04x}".format(pytest.tdc_id))
-    if config.getoption("--carrier") == "spec":
-        dev_dbg = re.search("spec-([0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}.[0-9a-f])",
-                            full_path).group(1)
-    path = "/sys/kernel/debug/{:s}/fpga_firmware".format(dev_dbg)
-    proc = subprocess.Popen(["echo -n {:s} > {:s}".format(os.path.basename(bit), path)], shell=True)
-    proc.wait()
-
 def pytest_configure(config):
     pytest.tdc_id = config.getoption("--tdc-id")
     pytest.fd_id = config.getoption("--fd-id")
-
-    # configure the kernel for reporting everything on dmesg
-    proc = subprocess.Popen(["echo 8 > /proc/sys/kernel/printk"], shell=True)
-    proc.wait()
-    bit = config.getoption("--tdc-bitstream")
-    if bit is not None:
-        proc = subprocess.Popen(["echo -n {:s} > /sys/module/firmware_class/parameters/path".format(os.path.dirname(bit))],
-                                shell=True)
-
-    fmctdc_configure_drivers(config)
-    fmctdc_configure_bitstream(config)
