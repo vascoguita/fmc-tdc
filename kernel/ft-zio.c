8a055dd537be193fe19427cc1b87db5941b453b3
Federico Vaga <federico.vaga@cern.ch>
2020-05-05 00:35:54 +0200

    drv: show all samples on debug mode
    Signed-off-by: Federico Vaga <federico.vaga@cern.ch>

--- kernel/ft-zio.c
+++ kernel/ft-zio.c
@@ -587,24 +587,12 @@ static int ft_trig_data_done(struct zio_cset *cset)
 		goto out;
 
 	ts = cset->chan->active_block->data;
-
-	dev_dbg(&cset->head.dev, "%s TS 0/%d %d.%d.%d %d\n",
-		__func__, cset->ti->nsamples,
-		ts[0].seconds,ts[0].coarse, ts[0].frac,
-		FT_HW_TS_META_SEQ(ts[0].metadata));
-	dev_dbg(&cset->head.dev, "%s TS %d/%d %d.%d.%d %d\n",
-		__func__, cset->ti->nsamples - 1, cset->ti->nsamples,
-		ts[cset->ti->nsamples - 1].seconds,
-		ts[cset->ti->nsamples - 1].coarse,
-		ts[cset->ti->nsamples - 1].frac,
-		FT_HW_TS_META_SEQ(ts[cset->ti->nsamples - 1].metadata));
-
-
 	for(i = 0; i < cset->ti->nsamples; ++i) {
-		dev_vdbg(&cset->head.dev, "%s TS  %d/%d %d.%d.%d %d\n",
+		dev_dbg(&cset->head.dev, "%s TS  %d/%d %d.%d.%d %d %d\n",
 			__func__, i, cset->ti->nsamples,
 			ts[i].seconds,ts[i].coarse,
-			ts[i].frac, FT_HW_TS_META_SEQ(ts[i].metadata));
+			ts[i].frac, FT_HW_TS_META_CHN(ts[i].metadata),
+			FT_HW_TS_META_SEQ(ts[i].metadata));
 	}
 	ft_zio_update_ctrl(cset, &ts[0]);
 
