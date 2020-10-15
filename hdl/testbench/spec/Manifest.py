e4906daba5f122e1e29d3ec7020a564fe1565155
Tristan Gingold <tristan.gingold@cern.ch>
2020-10-15 17:12:20 +0200

    testbench/spec: adjust Manifest.py

--- hdl/testbench/spec/Manifest.py
+++ hdl/testbench/spec/Manifest.py
@@ -3,11 +3,16 @@ top_module="main"
 syn_device="xc6slx45t"
 sim_top="main"
 
+if locals().get('fetchto', None) is None:
+  fetchto = "../../ip_cores"
+
 action = "simulation"
 target = "xilinx"
-fetchto = "../../ip_cores"
 include_dirs=[ "../../sim", "../include"  ]
 vcom_opt = "-mixedsvvh l"
+
+# For wr-cores
+board='spec'
 
 include_dirs = [
     "../include",
@@ -23,10 +28,23 @@ include_dirs = [
 files = [
     "main.sv",
     "buildinfo_pkg.vhd",
+    "sourceid_wr_spec_tdc_pkg.vhd",
 ]
 
 
-modules = { "local" :  [ "../../top/spec", "../../ip_cores/gn4124-core/hdl/sim/gn4124_bfm" ] }
+modules = {
+  "local" :  [
+    "../../top/spec",
+    fetchto + "/gn4124-core/hdl/sim/gn4124_bfm"
+  ],
+  "git" : [
+    "https://ohwr.org/project/wr-cores.git",
+    "https://ohwr.org/project/general-cores.git",
+    "https://ohwr.org/project/gn4124-core.git",
+    "https://ohwr.org/project/ddr3-sp6-core.git",
+  ],
+  "system": ['xilinx']
+}
 
 ctrls = ["bank3_32b_32b"]
 
@@ -35,3 +53,12 @@ try:
   exec(open(fetchto + "/general-cores/tools/gen_buildinfo.py").read())
 except:
   pass
+
+try:
+    # Assume this module is in fact a git submodule of a main project that
+    # is in the same directory as general-cores...
+    exec(open(fetchto + "/general-cores/tools/gen_sourceid.py").read(),
+         None, {'project': 'wr_spec_tdc'})
+except Exception as e:
+    print("Error: cannot generate source id file")
+    raise
