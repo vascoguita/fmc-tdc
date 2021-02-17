fc84e576e640b3fa1d52182bfe40597ad5dd525c
Tristan Gingold <tristan.gingold@cern.ch>
2021-02-17 08:53:20 +0100

    svec: move constraints file to syn/svec directory

--- hdl/top/svec/Manifest.py
+++ hdl/top/svec/Manifest.py
@@ -1,17 +1,16 @@
-files = ["wr_svec_tdc.ucf",
-"wr_svec_tdc.vhd"];
+files = ["wr_svec_tdc.vhd"];
 
 fetchto = "../../ip_cores"
 
 modules = {
     "local" : [
         "../../rtl/", 
-		"../../ip_cores/general-cores",
-		"../../ip_cores/vme64x-core",
-		"../../ip_cores/wr-cores",
+	"../../ip_cores/general-cores",
+	"../../ip_cores/vme64x-core",
+	"../../ip_cores/wr-cores",
         "../../ip_cores/wr-cores/board/svec",
         "../../ip_cores/svec",
         "../../ip_cores/ddr3-sp6-core"
-	    ]
-    }
+    ]
+}
 
