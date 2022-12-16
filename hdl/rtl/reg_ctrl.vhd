0cf850c5bc066e17dcf6a7f4618e376ecbc8102a
Dimitris Lampridis <dimitris.lampridis@cern.ch>
2022-12-16 14:26:16 +0100

    hdl: fix wrong reset logic for control register

--- hdl/rtl/reg_ctrl.vhd
+++ hdl/rtl/reg_ctrl.vhd
@@ -398,7 +398,7 @@ begin
   process (clk_tdc_i)
   begin
     if rising_edge (clk_tdc_i) then
-      if rst_tdc_n_i = '1' then
+      if rst_tdc_n_i = '0' then
         ctrl_reg_d <= (others =>'0');
       else
         ctrl_reg_d <= ctrl_reg;
